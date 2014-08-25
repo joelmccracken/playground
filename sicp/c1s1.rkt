281dbbcc82c3edfb66599ae2cac9ac305591bcf4
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-08-24 23:40:57 -0400

    ex 1.6 and part of 1.7

--- sicp/c1s1.rkt
+++ sicp/c1s1.rkt
@@ -1,7 +1,17 @@
-#lang racket/base
+#lang racket
 
 (require rackunit
          rackunit/text-ui)
+
+(define (check-does-not-finish-speedily thunk secs)
+  (let ([checker-thread (thread thunk)])
+    (for ([i (in-range secs)])
+         (if (not (thread-running? checker-thread))
+             (raise "executed speedily")
+             '())
+         (sleep 1))
+    (kill-thread checker-thread)))
+
 
 (define ex-1.1-tests
   (test-suite
@@ -75,12 +85,7 @@
   )
 
 #|
-Exercise 1.4.  Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
-
-  (define (a-plus-abs-b a b)
-    ((if (> b 0) + -) a b))
-
-Answer:
+Exercise 1.4. Answer:
 
 Since the operator (if...) either returns a + or a -,
 if b < 0, the expression becomes (- a b)
@@ -91,28 +96,7 @@ Either case is equivalent to "a plus the abs value of b"
 
 
 #|
-Exercise 1.5.  Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is using applicative-order
-evaluation or normal-order evaluation. He defines the following two procedures:
-
-(define (p) (p))
-
-(define (test x y)
-  (if (= x 0)
-      0
-      y))
-
-Then he evaluates the expression
-
-(test 0 (p))
-
-What behavior will Ben observe with an interpreter that uses applicative-order evaluation? What
-behavior will he observe with an interpreter that uses normal-order evaluation? Explain your
-answer. (Assume that the evaluation rule for the special form if is the same whether the
-interpreter is using normal or applicative order: The predicate expression is
-evaluated first, and the result determines whether to evaluate the consequent or the alternative expression.)
-
-
-Answer:
+Exercise 1.5. Answer:
 
 In the applicative case, `test` will evaluate both 0 and (p). However, (p) will loop infinitely.
 In the normal order case, the evaluation will follow:
@@ -130,6 +114,89 @@ because the p inside would need to be expanded. I guess this is why y combinator
 |#
 
 
+#|
+Exercise 1.6 Answer:
+
+new-if will evaluate both the `then` and `else` clauses. Thus, sqrt-iter is called
+infinitely, because calling it once forces it to be called again, and again, etc.
+|#
+
+(define ex-1.7-tests
+  (test-suite
+   "`good-enough?` precision tests"
+   (let ()
+     ;; define methods from the text
+     (define (sqrt-iter guess x)
+       (if (good-enough? guess x)
+           guess
+           (sqrt-iter (improve guess x)
+                      x)))
+
+     (define (improve guess x)
+       (average guess (/ x guess)))
+
+     (define (average x y)
+       (/ (+ x y) 2))
+
+     (define (sqrt x)
+       (sqrt-iter 1.0 x))
+
+     ;; throughout this test good-enough? will be
+     ;; redefined many times. here is the initial version
+     (define (good-enough? guess x)
+       (< (abs (- (square guess) x)) 0.001))
+
+     ;; there are two cases under discussion here
+     ;; the first is finding the square root of a very small number.
+     ;; the problem lies in the tolerance setting TS, above set to
+     ;; 0.001. When the X we are looking for in (sqrt X) is little,
+     ;; the X - (square guess) has a wide range of possible solutions,
+     ;; all less than TS.
+     ;; here is an example that illustrates the problem
+
+     ;; note: this might fail on a different platform, because the
+     ;; exact answer would be different
+     (let ((incorrect-root-answer 0.03230844833048122))
+       ;; the answer listen in the let above is incorrect. Here we see
+       ;; that it is what the sqrt method returns
+       (check-equal? (sqrt 0.0001) incorrect-root-answer)
+
+       ;; and here we see that the square of this provided root is
+       ;;                         very different from 0.0001
+       (check-equal? (square incorrect-root-answer)   0.0010438358335233748))
+
+     ;; The second case under discussion is when limited precision
+     ;; numbers are used for very large integers. This is made
+     ;; complicated by using Racket to work through these
+     ;; examples, as Racket has its own interesting number system.
+     ;; In Racket, when large, inexact numbers are in play, these are
+     ;; stored as exponents and addition operations do not store
+     ;; precise detail -- thus the guess will never be within the TS,
+     ;; as no guess will store enough detail. So, we should expect the
+     ;; algorithm to infinitely recurse. The exception to this is
+     ;; that if the square root of X we are solving for happens to be
+     ;; guessed, and that X is also precisely one of these numbers
+     ;; that a large number is stored as, this algorithm should still
+     ;; work.
+     (check-exn exn:fail?
+                (check-does-not-finish-speedily
+                 (thunk
+                  (let ([really-large-number 110798789796967820678678678729834.0])
+                    (check-equal? (sqrt really-large-number) 10)))
+                 0.1))
+
+     ;; the next piece of this -- writing a better `good-enough?` --
+     ;; still needs to be done. The general idea is to check to see if
+     ;; forward progress is being made, and otherwise return the results.
+
+
+     )))
+
+;; (require rackunit/gui)
+
+
+
 (run-tests ex-1.1-tests)
 (run-tests ex-1.2-tests)
 (run-tests ex-1.3-tests)
+(run-tests ex-1.7-tests)
