40edaa3b9844168967c0133f42c1dc4688370204
Joel McCracken <mccracken.joel@gmail.com>
2017-10-20 20:45:21 -0400

    haskellbook alg datatypes chapter

--- haskellbook/src/Lib.hs
+++ haskellbook/src/Lib.hs
@@ -1,8 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
 import qualified Algebra14
+import qualified BinaryTree11_14
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+run :: IO ()
+run = do
+  BinaryTree11_14.testMapTree
+  BinaryTree11_14.testPreorder
+  BinaryTree11_14.testInorder
+  BinaryTree11_14.testPostorder
+  BinaryTree11_14.testFoldTree
+  putStrLn "Tests done!"
