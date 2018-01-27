c0afe3634c2af5657d484bfc038661ddd6d1e830
Joel McCracken <mccracken.joel@gmail.com>
2018-01-27 12:27:16 -0500

    some chapter 13 exercises... lol just text

---
+++ haskellbook/src/Chapter13.hs
@@ -0,0 +1,10 @@
+module Chapter13 where
+
+-- check for understanding
+-- 1. forever, when
+-- 2. unqualified: mask, try, forever, when, all of data.bits, fromlistbe, chunksof, database.blacktip.types, unsafeperformio. Entirety: Data.bits, Database.blacktip.types
+-- 3. the variety of types used internally for the Blacktype library
+-- 4. a. Control.Concurrent.MVar, Filesystem.Path.CurrentOS, Control.Concurrent
+--    b. import qualified Filesystem as FS
+--    c. import Control.Monad (forever, when)
+--
