b64e3d650592843d84d0b92e200a3232705f6ab1
Joel McCracken <mccracken.joel@gmail.com>
2018-01-27 20:07:03 -0500

    gimmie person exercise

--- haskellbook/src/Chapter13.hs
+++ haskellbook/src/Chapter13.hs
@@ -74,7 +74,44 @@ runPalindromeBetter = do
   palindromeBetter
 
 
+-- 4
+
+type Name = String
+type Age = Integer
+data Person = Person Name Age
+  deriving Show
+
+data PersonInvalid
+  = NameEmpty
+  | AgeTooLow
+  | PersonInvalidUnknown String
+  deriving (Eq, Show)
+mkPerson :: Name
+         -> Age
+         -> Either PersonInvalid Person
+mkPerson name age
+  | name /= "" && age > 0 = Right $ Person name age
+  | name == "" = Left NameEmpty
+  | not (age > 0) = Left AgeTooLow
+  | otherwise = Left $ PersonInvalidUnknown
+    $ "Name was: " ++ show name ++ " Age was: " ++ show age
+
+gimmePerson :: IO ()
+gimmePerson = do
+  putStrLn "person name: "
+  name <- getLine
+  putStrLn "person age: "
+  sAge <- getLine
+  let age = read sAge :: Integer
+  case (mkPerson name age) of
+    Left error -> do
+      putStrLn $ "an error occurred: " ++ show error
+
+    Right person -> do
+      putStrLn $ "Yay! Successfully got a person: " ++ show person
+
 run = do
   -- runCipher
   -- runPalindrome
-  runPalindromeBetter
+  -- runPalindromeBetter
+  gimmePerson
