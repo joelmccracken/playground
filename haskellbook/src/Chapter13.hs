eec38d70fdffb5c10e0168c5dee31556c0150802
Joel McCracken <mccracken.joel@gmail.com>
2018-01-27 19:54:53 -0500

    palindrome exercises

--- haskellbook/src/Chapter13.hs
+++ haskellbook/src/Chapter13.hs
@@ -3,6 +3,9 @@ module Chapter13 where
 import qualified Chapter09 as C09
 import qualified Chapter11 as C11
 
+import System.Exit (exitSuccess)
+import Control.Monad
+import Data.Char
 -- check for understanding
 -- 1. forever, when
 -- 2. unqualified: mask, try, forever, when, all of data.bits, fromlistbe, chunksof, database.blacktip.types, unsafeperformio. Entirety: Data.bits, Database.blacktip.types
@@ -33,10 +36,45 @@ vigenereIO = do
   return (C11.vigenere cipher msg)
 
 
-run = do
+runCipher = do
   putStrLn "Running Caesar"
   c <- caesarIO
   putStrLn $ "done Running Caesar: " ++ c
   putStrLn "Running vigenere"
   v <- vigenereIO
   putStrLn $ "done Running vigenere: " ++ v
+
+-- 2
+
+palindrome :: IO ()
+palindrome = forever $ do
+  line1 <- getLine
+  case (line1 == reverse line1) of
+    True -> putStrLn "It's a palindrome!"
+    False -> do putStrLn "Nope!"
+                exitSuccess
+
+runPalindrome = do
+  putStrLn "palindrome test:"
+  palindrome
+
+-- 3
+
+palindromeBetter :: IO ()
+palindromeBetter = forever $ do
+  line1 <- getLine
+  let normalized = filter isAlpha $ map toLower line1
+  case (normalized == reverse normalized) of
+    True -> putStrLn "It's a palindrome!"
+    False -> do putStrLn "Nope!"
+                exitSuccess
+
+runPalindromeBetter = do
+  putStrLn "palindrome test:"
+  palindromeBetter
+
+
+run = do
+  -- runCipher
+  -- runPalindrome
+  runPalindromeBetter
