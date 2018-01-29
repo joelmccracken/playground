30a15401db182c1a1bf87c16cdead3f9c49b23d8
Joel McCracken <mccracken.joel@gmail.com>
2018-01-28 19:55:44 -0500

    puts that specs stuff in the source dirs

--- haskellbook/src/Lib.hs
+++ haskellbook/src/Lib.hs
@@ -5,6 +5,7 @@ import qualified Chapter10
 import qualified Chapter11
 import qualified Chapter12
 import qualified Chapter13
+import qualified Chapter14
 
 run :: IO ()
 run = do
@@ -12,5 +13,6 @@ run = do
   -- Chapter10.test
   -- Chapter11.runTests
   -- Chapter12.runTests
-  Chapter13.run
+  -- Chapter13.run
+  Chapter14.main
   putStrLn "done!"
