2dfbb72d6d399b7c48fe6b75f2624b9b67e14d1b
Joel McCracken <mccracken.joel@gmail.com>
2018-05-23 17:00:11 -0400

    adding EitherT

--- haskellbook/src/Chapter26.hs
+++ haskellbook/src/Chapter26.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Chapter26 where
+
+-- import Test.QuickCheck.Checkers
+-- import Test.QuickCheck.Classes
+
+-- import Data.Either
+import Test.Hspec
+
 
 newtype MaybeT m a =
   MaybeT { runMaybeT :: m (Maybe a) }
@@ -34,3 +43,46 @@ bindMaybeT (MaybeT ma) fammb =
         (return Nothing)
         (runMaybeT . fammb)
   in MaybeT $ ma >>= maybeConvert
+
+
+-- EitherT
+
+newtype EitherT e m a =
+  EitherT { runEitherT :: m (Either e a) }
+
+instance Show (EitherT e m a) where
+  show _ = "EitherT"
+
+-- 1
+
+instance Functor m =>
+         Functor (EitherT e m) where
+  fmap = eitherTFmap
+
+eitherTFmap :: Functor m
+            => (a -> b)
+            -> EitherT e m a
+            -> EitherT e m b
+eitherTFmap fab (EitherT meea) =
+  EitherT $ (fmap . fmap) fab meea
+
+-- 2
+
+instance Applicative m =>
+         Applicative (EitherT e m) where
+  pure = EitherT . pure . Right
+
+  f <*> a = undefined
+
+
+main = hspec $ do
+  describe "EitherT" $ do
+    it "functor" $ do
+      let
+        e :: EitherT String [] Int
+        e  = (+1) <$> (EitherT [Left "error"])
+      let
+        e' :: EitherT String [] Int
+        e' = (+1) <$> (EitherT [Right 2])
+      runEitherT e `shouldBe` [Left "error"]
+      runEitherT e' `shouldBe` [Right 3]
