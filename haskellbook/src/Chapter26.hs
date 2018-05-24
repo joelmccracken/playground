843e23a7187ed263198c4ccc2a0f7de139c40be5
Joel McCracken <mccracken.joel@gmail.com>
2018-05-24 16:56:25 -0400

    EitherT implemented

--- haskellbook/src/Chapter26.hs
+++ haskellbook/src/Chapter26.hs
@@ -72,8 +72,60 @@ instance Applicative m =>
          Applicative (EitherT e m) where
   pure = EitherT . pure . Right
 
-  f <*> a = undefined
+  (<*>) = applyEitherT
 
+applyEitherT :: Applicative m
+             => EitherT e m (a -> b)
+             -> EitherT e m a
+             -> EitherT e m b
+applyEitherT (EitherT mfab) (EitherT ma) =
+  let
+    x = 1
+  in
+    EitherT $ (<*>) <$> mfab <*> ma
+
+-- 3
+
+instance Monad m =>
+         Monad (EitherT e m) where
+  return = pure
+
+  (>>=) = bindEitherT
+
+bindEitherT :: Monad m
+            => EitherT e m a
+            -> (a -> EitherT e m b)
+            -> EitherT e m b
+bindEitherT (EitherT mea) faeb =
+  EitherT $ do
+    ea <- mea
+    case ea of
+      Left e -> pure (Left e)
+      Right a -> runEitherT (faeb a)
+
+-- 4
+
+swapEitherT :: (Functor m)
+            => EitherT e m a
+            -> EitherT a m e
+
+swapEitherT (EitherT (meea)) =
+  let
+    swap :: (Either e a) -> Either a e
+    swap (Left e) = Right e
+    swap (Right a) = Left a
+  in
+    EitherT $ fmap swap meea
+
+-- 5
+
+eitherT :: (Functor m, Monad m)
+        => (a -> m c)
+        -> (b -> m c)
+        -> EitherT a m b
+        -> m c
+eitherT amc bmc (EitherT meab) =
+  (meab >>= (either amc bmc))
 
 main = hspec $ do
   describe "EitherT" $ do
@@ -86,3 +138,30 @@ main = hspec $ do
         e' = (+1) <$> (EitherT [Right 2])
       runEitherT e `shouldBe` [Left "error"]
       runEitherT e' `shouldBe` [Right 3]
+
+    it "applicative" $ do
+      let
+        e :: EitherT String [] Int
+        e  = pure (5 :: Int)
+        a  = pure (+7)
+      runEitherT e `shouldBe` [Right 5]
+      runEitherT (a <*> e) `shouldBe` [Right 12]
+
+    it "monad" $ do
+      let
+        e :: EitherT String [] Int
+        e  = pure (5 :: Int)
+        f a = pure $ a + 11
+      runEitherT (e >>= f) `shouldBe` [Right 16]
+
+    it "swapEitherT" $ do
+      let
+        e :: EitherT String [] Int
+        e  = pure (5 :: Int)
+      runEitherT (swapEitherT e) `shouldBe` [Left 5]
+
+    it "eitherT" $ do
+      let
+        e :: EitherT Int [] Int
+        e  = pure (5 :: Int)
+      (eitherT pure pure e) `shouldBe` [5]
