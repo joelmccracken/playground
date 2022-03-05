369915044db1b0bd310fb61deefd46b8c97937a2
Joel McCracken <mccracken.joel@gmail.com>
2022-03-04 22:58:09 -0500

    sofware foundations code

---
+++ software-foundations/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "coq dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            coq
+          ];
+        };
+      });
+}
